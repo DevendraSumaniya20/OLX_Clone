84ac07d70e853bbc9f6c6306670bc1f3567e04bb
Sumaniya Devendra <dsumniya@gmail.com>
2023-09-18 19:12:01 +0530

    add new changes in files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import PostReducer from './PostSlice';
 import wishListReducer from './WishListSlice';
+import {persistStore, persistReducer} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
